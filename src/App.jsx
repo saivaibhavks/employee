212029c7c1944460b22547dc6de494f3ac46f801
Saivaibhav K S (OSV) <saivaibhav.s.osv@fedex.com>
2024-11-09 19:26:46 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { data } from "./assets/data";
 import Folder from "./Folder";
 import Employee from "./Employee.jsx";
+import { products } from "./assets/items.jsx"
 
 function App() {
   return (
     <div>
-      <Employee/>
+      <Employee item={products} />
     </div>
   )
 }
---
+++ src/assets/items.jsx
@@ -0,0 +1,11 @@
+export const products = [
+    { id: 1, name: "Laptop", price: 1200 },
+    { id: 2, name: "Headphones", price: 150 },
+    { id: 3, name: "Smartphone", price: 800 },
+    { id: 4, name: "Keyboard", price: 100 },
+    { id: 6, name: "Mouse2", price: 50 },
+    { id: 7, name: "Lapto3", price: 1200 },
+    { id: 8, name: "Headphones4", price: 150 },
+    { id: 9, name: "Smartphone5", price: 800 },
+    { id: 10, name: "Keyboard6", price: 100 },
+];
